036802dca65599083819e5a6f0867bb664ec7a55
Moein Karimi <moein.karimi76@yahoo.com>
2025-07-02 21:27:54 +0330

    Update ApiClient.java

--- app/src/main/java/com/example/karimimo/testservice/Retrofit/ApiClient.java
+++ app/src/main/java/com/example/karimimo/testservice/Retrofit/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL="http://185.173.168.134:82/";
+    private static final String BASE_URL="http://15.73.18.34:82/";
     private static Retrofit retrofit=null;
 
 
